4df1089c94dc0ced37ac3b11a82393a604e33dcc
nikhildambal <nikdambal007@gmail.com>
2021-07-29 13:23:14 +0530

    secondChange

--- Jenkinsdemo/src/com/phoenix/MainJen.java
+++ Jenkinsdemo/src/com/phoenix/MainJen.java
@@ -5,6 +5,7 @@ public class MainJen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello Jenkins");
+		System.out.println("some changes");
 
 	}
 
