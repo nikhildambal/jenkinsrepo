c9806049c7c234e9ca648e7537fd95db527a227d
nikhildambal <nikdambal007@gmail.com>
2021-07-29 15:18:24 +0530

    2nd project

---
+++ JenkinsMaven/src/main/java/JenkinsMaven/JenkinsMaven/App.java
@@ -0,0 +1,13 @@
+package JenkinsMaven.JenkinsMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JenkinsMaven/src/main/java/com/phoenix/MainC.java
@@ -0,0 +1,11 @@
+package com.phoenix;
+
+public class MainC {
+
+	public static void main(String[] args) {
+		System.out.println("hello Jenkins");
+		System.out.println("some changes");
+
+	}
+
+}
